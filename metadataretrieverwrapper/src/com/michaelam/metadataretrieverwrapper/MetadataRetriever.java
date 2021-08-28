0d69c0cc511a7eef789d09a915b3672a39d30e8e
LightingGamesPvtLmt <53119387+LightingGamesPvtLmt@users.noreply.github.com>
2021-08-28 03:37:30 +0300

    Feature update
    Auto development feature added/asset support is back, and now automatic!

--- metadataretrieverwrapper/src/com/michaelam/metadataretrieverwrapper/MetadataRetriever.java
+++ metadataretrieverwrapper/src/com/michaelam/metadataretrieverwrapper/MetadataRetriever.java
@@ -1,6 +1,7 @@
 
 package com.michaelam.metadataretrieverwrapper;
 
+import android.util.Log;
 import com.google.appinventor.components.annotations.DesignerComponent;
 import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.annotations.SimpleObject;
@@ -8,6 +9,11 @@ import com.google.appinventor.components.annotations.UsesPermissions;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.runtime.*;
 import android.content.Context;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 
 @DesignerComponent(
@@ -28,11 +34,52 @@ public class MetadataRetriever extends AndroidNonvisibleComponent {
     }
 
     @SimpleFunction(description = "Retrieves media information")
-    public String ExtractMediaMetaData(String fullFileName, int keyConstant)  {
+    public String ExtractMediaMetaData(String fullFileName, int keyConstant) {
         android.media.MediaMetadataRetriever metaRetriever = new android.media.MediaMetadataRetriever();
-            metaRetriever.setDataSource(fullFileName);
+        File file = new File(fullFileName);
+        if (!file.exists() && !file.getAbsolutePath().contains("/")) {
+            if (isDevelopment()) {
+                File developmentAsset = new File(context.getExternalFilesDir(null).getAbsolutePath() + "/AppInventor/assets/", fullFileName);
+                if (developmentAsset.exists()) {
+                    file = new File(developmentAsset.getAbsolutePath());
+                } else {
+                    Log.e("AssetExtractor", "ERROR! COULD NOT FIND ASSET.");
+                }
+            } else {
+                try {
+                    file = new File(ExtractAssetToCache(fullFileName).getAbsolutePath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+            metaRetriever.setDataSource(file.getAbsolutePath());
             String metaData = metaRetriever.extractMetadata(keyConstant);
             metaRetriever.release();
             return metaData;
+    }
+
+    public static File ExtractAssetToCache(String file) throws java.io.IOException {
+        File cacheFile = new File(context.getCacheDir(), file);
+        try {
+            try (InputStream inputStream = context.getAssets().open(file)) {
+                try (FileOutputStream outputStream = new FileOutputStream(cacheFile)) {
+                    byte[] buf = new byte[1024];
+                    int len;
+                    while ((len = inputStream.read(buf)) > 0) {
+                        outputStream.write(buf, 0, len);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new IOException("Could not open asset/asset does not exist", e);
+        }
+        return cacheFile;
+    }
+
+    public Boolean isDevelopment()
+    {
+        return context.getFilesDir().getAbsolutePath().contains("companion");
     }
 }
